ef683f98551124fafb2d3e5fa511f9d522be0b48
Estudiante <estudiante@iMac1s-iMac.local>
2018-12-13 19:43:17 -0400

    Initial Commit

---
+++ foodStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  foodStore
+//
+//  Created by Estudiante on 12/13/18.
+//  Copyright © 2018 ucb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
